ccfc0c761240fb820f828b16ec3fa723291dadc9
Suchith Nayaka <Suchithnayaka>
2022-01-28 09:45:24 +0530

    Initial Commit

---
+++ Api/ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiApp.swift
+//  Api
+//
+//  Created by Suchith Nayaka on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Api/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  Api
+//
+//  Created by Suchith Nayaka on 22/01/22.
+//
+
+import SwiftUI
+
+struct URLImage: View{
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body:some View{
+        if let data = data, let uiimage=UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:130,height:70)
+                .background(Color.gray)
+            
+        }
+        else{
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear{
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _
+            in
+            self.data=data
+        }
+        task.resume()
+    }
+}
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack{
+                        URLImage(urlString: course.image)
+                        
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear{
+                viewModel.fetch()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
